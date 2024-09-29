ffb2ed6d7cc371e12c6b240257fc0ee50607eb9d
Ricardo Calixto <38529592+RiczCalixto@users.noreply.github.com>
2024-09-29 10:10:33 -0300

    Initial Commit

---
+++ code history/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  code history
+//
+//  Created by Taqtile on 29/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ code history/code_historyApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_historyApp.swift
+//  code history
+//
+//  Created by Taqtile on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct code_historyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
